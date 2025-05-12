7e8763b4df395b4618739177626bdffd7fe832f9
Ariel <108890536+achacona1998@users.noreply.github.com>
2025-05-11 23:22:29 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  base: '/nombre-del-repositorio/',
+  base: '/sportwebsite/',
   integrations: [react()],
 
   vite: {
